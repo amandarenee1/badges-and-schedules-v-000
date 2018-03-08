fba2ebd93c36e6499fd1607ad06cfdb3b4cd91cd
Amanda Wingate <arw0219@gmail.com>
2018-03-08 22:20:47 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,7 +12,7 @@ def batch_badge_creator(speakers)
   badge_messages
 end
 
-def assign_rooms(attendees, room_assignments)
+def assign_rooms(attendees)
   room_assignments = 1
   room_messages = []
   attendees.each do |attendee|

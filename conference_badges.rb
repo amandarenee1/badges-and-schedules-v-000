458646cebf9a931b79988188f5f27fd1bbef9609
Amanda Wingate <arw0219@gmail.com>
2018-03-08 22:25:12 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,8 +13,17 @@ def batch_badge_creator(speakers)
 end
 
 def assign_rooms(attendees)
-  room = 0
-  attendees.collect.do |attendee|
-  room += 1
-  return "Hello, #{attendee}! You'll be assigned to room #{room}!"
+  room=0
+  attendees.collect do |name|
+    room+=1
+    "Hello, #{name}! You'll be assigned to room #{room}!"
+  end
 end
+
+def printer(attendees)
+  batch_badge_creator(attendees).each do |value|
+    puts value
+  end
+  assign_rooms(attendees).each do |value|
+    puts value
+  end

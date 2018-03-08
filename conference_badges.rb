72e8c4b565e9644a59eaf300a6031f330c93c4ae
Amanda Wingate <arw0219@gmail.com>
2018-03-08 22:13:32 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,3 +11,13 @@ def batch_badge_creator(speakers)
   end
   badge_messages
 end
+
+def assign_rooms(speakers)
+  room_number = 1
+  room_messages = []
+  speakers.each do |speaker|
+    room_messages << "Hello, #{speaker}! You'll be to room #{room_number}!"
+    room_number += 1
+  end
+  room_messages
+end
